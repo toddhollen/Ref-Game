c661341c2a8cca8d5a84ee198ece6064d2883fe1
toddhollen <toddhollen@gmail.com>
2014-10-29 16:03:39 -0600

    Added some js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,88 +2,89 @@ $(document).ready(function () {
 
 	//Question Array
 		//Need to finish imputting pictures
+	var currentQuestionNum = 0;
 
 	var questions = [{
-        question: "Boarding"
-        choices: ["assets/images/refgamequestions/boarding.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", ],
+        question: "Boarding",
+        choices: ["assets/images/refgamequestions/boarding.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ "],
         qNum : 0,
         correct : 0,
-        description:
+        description: ''
         },
         {
         question: "Charging",
-        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", ]
+        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ "],
         qNum : 1,
         correct : 1,
-        description:
+        description:''
         },
         {
         question: "Goal",
-        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/goal.jpg ", ]
+        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/goal.jpg "],
         qNum : 2,
         correct : 3,
-        description:
+        description:''
         },
         {
         question: "High Sticking",
-        choices: ["assets/images/refgamequestions/high.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", ]
+        choices: ["assets/images/refgamequestions/high.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ "],
         qNum : 3,
         correct : 1,
-        description:
+        description:''
         },
         {
         question: "Delayed Offsides",
-        choices: ["assets/images/refgamequestions/ .jpg","assets/images/refgamequestions/.offside.jpg", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", ]
+        choices: ["assets/images/refgamequestions/ .jpg","assets/images/refgamequestions/.offside.jpg", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ "],
         qNum : 4,
         correct : 1,
-        description:
+        description:''
         },
         {
         question: "Roughing",
-        choices: ["assets/images/refgamequestions/fighting.jpg","assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg", "assets/images/refgamequestions/pshot.jpg", ]
+        choices: ["assets/images/refgamequestions/fighting.jpg","assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg", "assets/images/refgamequestions/pshot.jpg"],
         qNum : 5,
         correct : 1,
-        description:
+        description:''
         },
         {
         question: "Spearing",
-        choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg", "assets/images/refgamequestions/hooking.jpg", "assets/images/refgamequestions/spearing.jpg", ]
+        choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg", "assets/images/refgamequestions/hooking.jpg", "assets/images/refgamequestions/spearing.jpg"],
         qNum : 6,
         correct : 3,
-        description:
+        description:''
         },
         {
         question: "Washout",
-        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", ]
+        choices: ["assets/images/refgamequestions/.jpg","assets/images/refgamequestions/ ", "assets/images/refgamequestions/ ", "assets/images/refgamequestions/ "],
         qNum : 7,
         correct : 1,
-        description:
+        description:''
         },
         {
         question: "Hand Pass",
-        choices: ["assets/images/refgamequestions/holding.jpg","assets/images/refgamequestions/kneeing.jpg", "assets/images/refgamequestions/ioffside.jpg", "assets/images/refgamequestions/hand.jpg", ]
+        choices: ["assets/images/refgamequestions/holding.jpg","assets/images/refgamequestions/kneeing.jpg", "assets/images/refgamequestions/ioffside.jpg", "assets/images/refgamequestions/hand.jpg"],
         qNum : 8,
         correct : 3,
-        description:
+        description:''
         },
         {
         question: "Misconduct",
-        choices: ["assets/images/refgamequestions/misconduct.jpg","assets/images/refgamequestions/interference.jpg", "assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg", ]
+        choices: ["assets/images/refgamequestions/misconduct.jpg","assets/images/refgamequestions/interference.jpg", "assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg"],
         qNum : 9,
         correct : 0,
-        description:
+        description:''
         },
         {
         question: "Tripping",
-        choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg ", "assets/images/refgamequestions/holding-stick.jpg", "assets/images/refgamequestions/grasping.jpg", ]
+        choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg ", "assets/images/refgamequestions/holding-stick.jpg", "assets/images/refgamequestions/grasping.jpg"],
         qNum : 10,
         correct : 1,
-        description:
+        description:''
         }]
 
         //Global Variables
         var numberCorrect = 0;
-
+        var currentQuestionData = questions[currentQuestionNum]
 	// Intro Screen - Click to Start
 
 	$(".start").click(function(){
@@ -95,8 +96,18 @@ $(document).ready(function () {
 	// Game Play - 
 
 	// Step 1: Load first question into class "call"
+	//Get Content for Question
+$('#game').ready(function() {
+	
+	$('h4.call').html(currentQuestionData.questions);
 
 	// Load first 4 pictures into class "choices"
+	$('.answer1 img').attr('scr', currentQuestionData.choices[0]);
+	$('.answer2 img').attr('scr', currentQuestionData.choices[1]);
+	$('.answer3 img').attr('scr', currentQuestionData.choices[2]);
+	$('.answer4 img').attr('scr', currentQuestionData.choices[3]);
+
+	});
 
 	// One picture matches "call"
 
