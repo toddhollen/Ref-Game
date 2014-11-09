121ccba08d40855e7423bcca28c8eca1e01194c2
toddhollen <toddhollen@gmail.com>
2014-11-08 19:23:37 -0700

    Added answers, broken end screen code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 	//Question Array
-		
+		//Need to add Answer Pictures to each
 	currentQuestionNum = 0;
 
 	questions = [{
@@ -15,6 +15,7 @@
         choices: ["assets/images/refgamequestions/hooking.jpg","assets/images/refgamequestions/charging.jpg", "assets/images/refgamequestions/delay.jpg", "assets/images/refgamequestions/tripping.jpg"],
         qNum : 1,
         correct : 1,
+        correctImage : ["assets/images/refgameanswers/charging.jpg"],
         description:''
         },
         {
@@ -22,20 +23,23 @@
         choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/offside.jpg", "assets/images/refgamequestions/goal.jpg"],
         qNum : 2,
         correct : 3,
+        correctImage : ["assets/images/refgameanswers/goal.jpg"],
         description:''
         },
         {
         question: "High Sticking",
         choices: ["assets/images/refgamequestions/high.jpg","assets/images/refgamequestions/kneeing.jpg", "assets/images/refgamequestions/ioffside.jpg", "assets/images/refgamequestions/holding-stick.jpg"],
         qNum : 3,
-        correct : 1,
+        correct : 0,
+        correctImage : ["assets/images/refgameanswers/high.jpg"],
         description:''
         },
         {
         question: "Delayed Offsides",
-        choices: ["assets/images/refgamequestions/cross.jpg","assets/images/refgamequestions/.offside.jpg", "assets/images/refgamequestions/interference.jpg", "assets/images/refgamequestions/misconduct.jpg"],
+        choices: ["assets/images/refgamequestions/cross.jpg","assets/images/refgamequestions/offside.jpg", "assets/images/refgamequestions/interference.jpg", "assets/images/refgamequestions/misconduct.jpg"],
         qNum : 4,
         correct : 1,
+        correctImage : ["assets/images/refgameanswers/offside.jpg"],
         description:''
         },
         {
@@ -43,6 +47,7 @@
         choices: ["assets/images/refgamequestions/fighting.jpg","assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg", "assets/images/refgamequestions/pshot.jpg"],
         qNum : 5,
         correct : 1,
+        correctImage : ["assets/images/refgameanswers/roughing.jpg"],
         description:''
         },
         {
@@ -50,6 +55,7 @@
         choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg", "assets/images/refgamequestions/hooking.jpg", "assets/images/refgamequestions/spearing.jpg"],
         qNum : 6,
         correct : 3,
+        correctImage : ["assets/images/refgameanswers/spearing.jpg"],
         description:''
         },
         {
@@ -57,6 +63,7 @@
         choices: ["assets/images/refgamequestions/fighting.jpg","assets/images/refgamequestions/washout.jpg", "assets/images/refgamequestions/high.jpg", "assets/images/refgamequestions/spearing.jpg"],
         qNum : 7,
         correct : 1,
+        correctImage : ["assets/images/refgameanswers/washout.jpg"],
         description:''
         },
         {
@@ -64,6 +71,7 @@
         choices: ["assets/images/refgamequestions/holding.jpg","assets/images/refgamequestions/kneeing.jpg", "assets/images/refgamequestions/ioffside.jpg", "assets/images/refgamequestions/hand.jpg"],
         qNum : 8,
         correct : 3,
+        correctImage : ["assets/images/refgameanswers/hand.jpg"],
         description:''
         },
         {
@@ -71,6 +79,7 @@
         choices: ["assets/images/refgamequestions/misconduct.jpg","assets/images/refgamequestions/interference.jpg", "assets/images/refgamequestions/roughing.jpg", "assets/images/refgamequestions/slashing.jpg"],
         qNum : 9,
         correct : 0,
+        correctImage : ["assets/images/refgameanswers/misconduct.jpg"],
         description:''
         },
         {
@@ -78,17 +87,22 @@
         choices: ["assets/images/refgamequestions/timeout.jpg","assets/images/refgamequestions/tripping.jpg", "assets/images/refgamequestions/holding-stick.jpg", "assets/images/refgamequestions/grasping.jpg"],
         qNum : 10,
         correct : 1,
+        correctImage : ["assets/images/refgameanswers/tripping.jpg"],
         description:''
         }]
 
         //Global Variables
         var numberCorrect = 0;
         var currentQuestionData = questions[currentQuestionNum];
+
 	// Intro Screen - Click to Start
+    var getCurrentQuestionData = function(questions, currentQuestionNum){
+        return questions[currentQuestionNum];
+    }
 
     // Setting functions outside of document ready
-    function showCurrentquestion(currentQuestionData){
-    
+       function showCurrentquestion(currentQuestionData){
+
         //step 1: Clear Template
         $('h4.call').html("");
 
@@ -121,9 +135,21 @@ $(document).ready(function() {
 
     $(".return").click(function(){
         event.preventDefault();
+
+        // Bring in UI for Game
         $("#game").show("slow");
         $("#answer").hide("slow");
-        showCurrentquestion(currentQuestionData);
+
+        // Clear out original responses
+        $(".correct, .incorrect").hide();
+        $(".correct-image").attr('src', '');
+
+        // Get Data to display
+        var cqd = getCurrentQuestionData(questions, currentQuestionNum);
+
+        //Display Data
+        showCurrentquestion(cqd);
+                
     });
 	
     $('.choices a').on('click', function(event){
@@ -138,22 +164,27 @@ $(document).ready(function() {
 
         //Get the index number of clicked answer
         var index = $(".choices a").index(currentItem);
-        var currentQuestionData = questions[currentQuestionNum];
+        var cqd = getCurrentQuestionData(questions, currentQuestionNum);
 
-        var correct = currentQuestionData.correct;
-        var correctImage = currentQuestionData.correctImage;
+        var correct = cqd.correct;
+        var correctImage = cqd.correctImage;
+         if (index <= questions.length){
+            if (index == correct) {
+                $("#answer, .correct").show("slow");
+                $(".correct-image").attr('src', cqd.correctImage);
+                $("#game").hide("slow");  
+                ++currentQuestionNum;
+            } else {
+                $("#answer, .incorrect").show("slow");
+                $(".correct-image").attr('src', cqd.correctImage);
+                $("#game").hide("slow"); 
+                ++currentQuestionNum;
+            }
+        }else {
+                //Show end of the game
+                $("#answer, .correct, .incorrect, #game").hide("slow");
+                $("#end").show("slow");  
 
-        if (index == correct) {
-            $("#answer, .correct").show("slow");
-            $(".correct-image").attr('src', currentQuestionData.correctImage);
-            $("#game").hide("slow");  
-            currentQuestionNum++;
-        } else {
-            $("#answer, .incorrect").show("slow");
-            $(".correct-image").attr('src', currentQuestionData.correctImage);
-            $("#game").hide("slow"); 
-            currentQuestionNum++;
         }
     });
-
 });
